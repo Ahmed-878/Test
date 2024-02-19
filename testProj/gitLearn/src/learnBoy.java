734fbb77a935e9fd4cc114a05dc00355fde3a14f
Ahmed-878 <ahmedabouelhaggag06@gmail.com>
2024-02-19 14:59:17 +0200

    Checking wut will happen to the same file in the master branch

--- testProj/gitLearn/src/learnBoy.java
+++ testProj/gitLearn/src/learnBoy.java
@@ -1,6 +1,7 @@
 public class learnBoy {
     public static void main(String[] args) throws Exception {
         System.out.println("Learning, Git!");
-        //Push 
+        //Push 1
+        System.out.println("Push 2");
     }
 }
